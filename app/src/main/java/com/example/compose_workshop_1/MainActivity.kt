d6a6781fa7fc32a9e91437b9fb9655ea0210be33
Abhinay <sahuabhinay317@gmail.com>
2025-11-14 17:10:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/compose_workshop_1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.compose_workshop_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_workshop_1.ui.theme.Compose_Workshop_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Compose_Workshop_1Theme {
+                Demo()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Demo() {
+    Text("Hello World!")
+}
